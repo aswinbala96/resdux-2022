f75d22f0a2bdc71b0a7bda4b3d481920992fd4a2
Aswin Balasubramaniam <ash4harekrsna@gmail.com>
2022-11-08 09:28:02 +0100

    Text Update v4

--- src/pages/deliverables.js
+++ src/pages/deliverables.js
@@ -41,14 +41,14 @@ const DeliverablesPage = () => {
                 <div className='del-content'>
                   <h3>Deliverables</h3>
                   <div className='del-bodytext'>
-                    Listed below are the released deliverable for RESDUX 2022. More will be posted here as the quarter progresses. Go to home page for new announcements.
+                    Listed below are the released deliverables for RESDUX 2022-23. More will be posted here as the quarter progresses. Visit Cavas for new announcements.
                   </div>
 
                   <h3>Final Deliverables</h3>
                   <div className='del-bodytext'>
                     <ul class= 'ul-style'>
                         <li style={{fontFamily: "Inconsolata, monospace"}}>
-                            <span class = 'highlight'>Due Date</span>: Friday, December 13, 20221 @23:59
+                            <span class = 'highlight'>Due Date</span>: Friday, February 3, 2023 @23:59
                         </li>
 
                         <li style={{fontFamily: "Inconsolata, monospace"}}>
@@ -62,8 +62,14 @@ const DeliverablesPage = () => {
                             </ul>
                         </li>
 
+
                         <li>
-                          <a href={"https://www.google.com"} target="_blank" className='textHover' style={{fontFamily: "Inconsolata, monospace", cursor: "pointer", color: "#1228e2"}}>Deliverable 3 Link</a>
+                          <a href={"https://www.google.com"} target="_blank" className='textHover' style={{fontFamily: "Inconsolata, monospace", cursor: "pointer", color: "#1228e2"}}>Final Deliverable Document Link</a>
+                        </li>
+
+
+                        <li>
+                          <a href={"https://www.google.com"} target="_blank" className='textHover' style={{fontFamily: "Inconsolata, monospace", cursor: "pointer", color: "#1228e2"}}>Final Deliverable Submission Link</a>
                         </li>
                     </ul>
                   </div>
@@ -72,7 +78,7 @@ const DeliverablesPage = () => {
                   <div className='del-bodytext'>
                     <ul class= 'ul-style'>
                         <li style={{fontFamily: "Inconsolata, monospace"}}>
-                            <span class = 'highlight'>Due Date</span>: Friday, December 13, 20221 @23:59
+                            <span class = 'highlight'>Due Date</span>: Friday, December 20, 2022 @23:59
                         </li>
 
                         <li style={{fontFamily: "Inconsolata, monospace"}}>
@@ -87,7 +93,67 @@ const DeliverablesPage = () => {
                         </li>
 
                         <li>
-                          <span className='textHover' style={{fontFamily: "Inconsolata, monospace", cursor: "pointer", color: "#1228e2"}}>Deliverable 3 Link</span>
+                          <span className='textHover' style={{fontFamily: "Inconsolata, monospace", cursor: "pointer", color: "#1228e2"}}>Deliverable 3 Document Link</span>
+                        </li>
+
+                        <li>
+                          <span className='textHover' style={{fontFamily: "Inconsolata, monospace", cursor: "pointer", color: "#1228e2"}}>Deliverable 3 Submission Link</span>
+                        </li>
+                    </ul>
+                  </div>
+
+                  <h3>Deliverable 2</h3>
+                  <div className='del-bodytext'>
+                    <ul class= 'ul-style'>
+                        <li style={{fontFamily: "Inconsolata, monospace"}}>
+                            <span class = 'highlight'>Due Date</span>: Friday, December 9, 2022 @23:59
+                        </li>
+
+                        <li style={{fontFamily: "Inconsolata, monospace"}}>
+                            Checklist of Item to be Included:
+                            <ul class= 'ul-style'>
+                              <li style={{fontFamily: "Inconsolata, monospace"}}>Item 1</li>
+                              <li style={{fontFamily: "Inconsolata, monospace"}}>Item 2</li>
+                              <li style={{fontFamily: "Inconsolata, monospace"}}>Item 3</li>
+                              <li style={{fontFamily: "Inconsolata, monospace"}}>Item 4</li>
+                              <li style={{fontFamily: "Inconsolata, monospace"}}>Item 5</li>
+                            </ul>
+                        </li>
+
+                        <li>
+                          <span className='textHover' style={{fontFamily: "Inconsolata, monospace", cursor: "pointer", color: "#1228e2"}}>Deliverable 2 Document Link</span>
+                        </li>
+                        
+                        <li>
+                          <span className='textHover' style={{fontFamily: "Inconsolata, monospace", cursor: "pointer", color: "#1228e2"}}>Deliverable 2 Submission Link</span>
+                        </li>
+                    </ul>
+                  </div>
+
+                  <h3>Deliverable 1</h3>
+                  <div className='del-bodytext'>
+                    <ul class= 'ul-style'>
+                        <li style={{fontFamily: "Inconsolata, monospace"}}>
+                            <span class = 'highlight'>Due Date</span>: Friday, November 25, 2022 @23:59
+                        </li>
+
+                        <li style={{fontFamily: "Inconsolata, monospace"}}>
+                            Checklist of Item to be Included:
+                            <ul class= 'ul-style'>
+                              <li style={{fontFamily: "Inconsolata, monospace"}}>Item 1</li>
+                              <li style={{fontFamily: "Inconsolata, monospace"}}>Item 2</li>
+                              <li style={{fontFamily: "Inconsolata, monospace"}}>Item 3</li>
+                              <li style={{fontFamily: "Inconsolata, monospace"}}>Item 4</li>
+                              <li style={{fontFamily: "Inconsolata, monospace"}}>Item 5</li>
+                            </ul>
+                        </li>
+
+                        <li>
+                          <span className='textHover' style={{fontFamily: "Inconsolata, monospace", cursor: "pointer", color: "#1228e2"}}>Deliverable 1 Document Link</span>
+                        </li>
+
+                        <li>
+                          <span className='textHover' style={{fontFamily: "Inconsolata, monospace", cursor: "pointer", color: "#1228e2"}}>Deliverable 1 Submission Link</span>
                         </li>
                     </ul>
                   </div>
